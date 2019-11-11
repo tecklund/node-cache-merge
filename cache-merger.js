6e73ae517595c202c7ffe59bd08d7f3d9a939bba
Timothy Ecklund <timothy.ecklund@sparkpost.com>
2019-11-11 11:32:42 -0500

    even better

--- cache-merger.js
+++ cache-merger.js
@@ -2,28 +2,37 @@
 
 const cache = module.exports;
 const hash = require('object-hash');
+cache.promise = Promise;
 
-const addPrefix = (prefix) => (key) => prefix+":"+hash(key);
+cache.addPrefix = (prefix) => (key) => `${prefix}:${key}`
+cache.compose = (f, g) => (args) => f(g(args))
+cache.identity = x => x
 
-cache.getLocal = (cache) => (key) => Promise.resolve(cache.get(key));
-cache.setLocal = (exp) => (cache) => (key) => (val) => Promise.resolve(cache.set(key, val, exp));
+cache.getLRU = (lru) => (key) => cache.promise.resolve(lru.get(key));
+cache.setLRU = (exp) => (lru) => (key) => (val) => cache.promise.resolve(lru.set(key, val, exp));
 
-cache.getRedis = (rclient) => (key) => rclient.getAsync(key).catch(() => undefined);
+const nullToUndefined = (val) => val === null ? undefined : val;
+
+cache.getRedis = (rclient) => (key) => rclient.getAsync(key).then(nullToUndefined).catch(() => undefined);
 cache.setRedis = (exp) => (rclient) => (key) => (val) => rclient.setexAsync(key, exp, val).catch(() => undefined);
 
-cache.namedStash = (name) => (cache) => (getter) => (setter) => ({
-  get: (key) => getter(cache)(addPrefix(name)(key)),
-  set: (key) => (val) => setter(cache)(addPrefix(name)(key))(val)
+cache.namedStash = (name) => 
+  cache.keyGenStash(cache.compose(cache.addPrefix(name), hash))
+
+cache.hashedStash = 
+  cache.keyGenStash(hash)
+
+cache.keyedStash = (name) =>
+  cache.keyGenStash(cache.addPrefix(name))
+
+cache.keyGenStash = (keyGen) => (getter, setter) => ({
+  get: (key) => getter(keyGen(key)),
+  set: (key) => (val) => setter(keyGen(key))(val)
 });
 
-const noSet = (key) => (val) => Promise.resolve();
+const noSet = (key) => (val) => cache.promise.resolve();
 
-cache.stash = (getter) => (setter = noSet) => ({
-    get: getter,
-    set: setter
-});
-
-cache.promise = Promise;
+cache.stash = (getter, setter = noSet) => cache.keyGenStash(cache.identity)(getter, setter)
 
 /** 
  * takes two caches and chains them so that if the first cache misses, the second one will 
@@ -31,16 +40,16 @@ cache.promise = Promise;
  */
 cache.combine = (stash1, stash2) => ({
     get: (key) => 
-            stash1.get(key)
-            .then((val) => val 
-              ? val 
-              : stash2.get(key)
-                .then((val) => val
-                  ? stash1.set(key)(val).then(() => val)
-                  : cache.promise.resolve(val)
-                ),
+      stash1.get(key).then((stash1_val) =>                      //attempt to get the key from stash1
+        stash1_val !== undefined                                //check if val from stash1 is undefined
+        ? stash1_val                                            //val was not undefined, return val
+        : stash2.get(key).then((stash2_val) =>                  //val was undefined, get val from stash2
+          stash2_val !== undefined                              //check if val from stash2 is undefined
+          ? stash1.set(key)(stash2_val).then(() => stash2_val)  //val from stash2 not undefined, set val in stash 1 and return val
+          : cache.promise.resolve(undefined)                    //val from stash2 was undefined, return undefined
         ),
+      ),
     set: (key) => (val) => stash1.set(key)(val).then(() => stash2.set(key)(val)).then(() => undefined)
   });
 
-cache.merge = (...caches) => caches.reduce((acc, next) => cache.combine(acc, next));
+cache.merge = (...stashes) => stashes.reduce(cache.combine);
--- cache-merger.test.js
+++ cache-merger.test.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const cache = require('./cache-merger');
+const Promise = require('bluebird');
+const hash = require('object-hash');
 
 test(`Given [0:undefined, 1:undefined, 2:val] 
       when get is called
@@ -45,4 +47,109 @@ test(`Given [0:undefined, 1:undefined, 2:val]
     expect(setmockC.mock.calls.length).toBe(0);
   })
   
+});
+
+test(`Given redis has no value for key A 
+      when getRedis is called for key A
+      then the result will be undefined, not null`, () => {
+
+  const redisMock = { getAsync: jest.fn((val) => Promise.resolve(undefined)) }
+  cache.getRedis(redisMock)('A').then((val) => {
+    expect(val).toBe(undefined)
+    expect(redisMock.getAsync.mock.calls.length).toBe(1)
+  })
+});
+
+
+test(`test code`, () => {
+
+    let a = undefined;
+    let b = undefined;
+    let c = 'smaugs treasure'
+    const stashA = ({
+      get: (key) => Promise.resolve(a),
+      set: (key) => (val) => Promise.resolve(a = val)
+    })
+    const stashB = ({
+      get: (key) => Promise.resolve(b),
+      set: (key) => (val) => Promise.resolve(b = val)
+    })
+    const stashC = ({
+      get: (key) => Promise.resolve(c),
+      set: (key) => (val) => Promise.resolve(c = val)
+    })
+    const stash = cache.merge(stashA, stashB, stashC)
+    stash.get("key").then((val) => console.log(val)).then(() => console.log(a))
+      
+});
+
+const LRU = require("lru-cache");
+const localcache = new LRU({ max: 70 });
+
+const redis = require('redis');
+Promise.promisifyAll(redis);
+
+const rclient = redis.createClient();
+const nullToUndefined = (val) => val === null ? undefined : val;
+test(`test caches`, async function() {
+  let c = 'smaugs treasure'
+  const stashA = ({
+    get: (key) => Promise.resolve(localcache.get(key)),
+    set: (key) => (val) => Promise.resolve(localcache.set(key, val, 1))
+  })
+  const stashB = ({
+    get: (key) => rclient.getAsync(key).then(nullToUndefined).catch(() => undefined),
+    set: (key) => (val) => rclient.setexAsync(key, 1, val).catch(() => undefined)
+  })
+  const stashC = ({
+    get: (key) => Promise.resolve(c),
+    set: (key) => (val) => Promise.resolve(c = val)
+  })
+  const stash = cache.merge(stashA, stashB, stashC)
+  await stash.get("key").then((v) => console.log(`ret val was ${v}`))
+    .tap(() => rclient.getAsync("key").then((v) => console.log(`redis result was ${v}`)))
+    .tap(() => rclient.quit())
+});
+
+
+const getLRU = (lru) => (key) => Promise.resolve(lru.get(key));
+const setLRU = (exp) => (lru) => (key) => (val) => Promise.resolve(lru.set(key, val, exp));
+
+const getRedis = (rclient) => (key) => rclient.getAsync(key).then(nullToUndefined).catch(() => undefined);
+const setRedis = (exp) => (rclient) => (key) => (val) => rclient.setexAsync(key, exp, val).catch(() => undefined);
+const noSet = (key) => (val) => Promise.resolve();
+
+const stash = (getter, setter = noSet) => ({
+  get: getter,
+  set: setter
+});
+
+test(`test caches`, async function() {
+
+
+  let c = 'smaugs treasure'
+  const stashA = stash(getLRU(localcache), setLRU(5)(localcache))
+  const stashB = stash(getRedis(rclient), setRedis(5)(rclient))
+  const stashC = stash(() => Promise.resolve(c))
+  const totalStash = cache.merge(stashA, stashB, stashC)
+  await totalStash.get("key").then((v) => console.log(`ret val was ${v}`))
+    .tap(() => rclient.quit())
+});
+
+
+test(`test caches`, async function() {
+  const addPrefix = (prefix) => (key) => prefix+":"+hash(key);
+  const namedStash = (name) => (getter, setter) => ({
+    get: (key) => getter(addPrefix(name)(key)),
+    set: (key) => (val) => setter(addPrefix(name)(key))(val)
+  });
+  const appstash = namedStash('myapp');
+
+  let c = 'smaugs treasure'
+  const stashA = stash(getLRU(localcache), setLRU(5)(localcache))
+  const stashB = appstash(getRedis(rclient), setRedis(5)(rclient))
+  const stashC = stash(() => Promise.resolve(c))
+  const totalStash = cache.merge(stashA, stashB, stashC)
+  await totalStash.get("key").then((v) => console.log(`ret val was ${v}`))
+    .tap(() => rclient.quit())
 });
